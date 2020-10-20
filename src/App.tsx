47608045d9b42e3a69876f9e94a9356f44fef100
Pedro Sérgio <www.psro@gmail.com>
2020-10-20 15:25:19 -0300

    App: updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,18 @@
 import React, { useState } from 'react';
-import Pixel from './components/Pixel';
+import Board from './components/Board';
+import Picture from './model/Picture';
 
 function App() {
 
-  const [ color, setColor ] = useState('#000000');
-  const [ showGrid, setShowGrid ] = useState(false);
-  const pixelQuantity = 16;
-  const pixelSize = 40 / pixelQuantity;
-  const [ pixels, setPixels ] = useState<string[]>(new Array(pixelQuantity ** 2).fill('#FFFFFF00'));
-
-  function handlePainting(location: number) {
-    setPixels(pixels.map((pixel, index) => index === location? color : pixel));
-  }
+  const [picture, setPicture] = useState(Picture.empty(100, 100, '#ffffff00'))
 
   return (
-    <div className="App">
-      <div className="board">
-
-        { pixels.map( (pixel, index) => (
-          <Pixel
-            key={index}
-            index={index}
-            paint={handlePainting} 
-            size={pixelSize} 
-            color={pixel}
-            isGrid={showGrid}
-          />
-        )) }
-      
+    <div className="app">
+      <div className="tools">
+        <div className="tool" onClick={() => setPicture(Picture.empty(100, 100, '#ffffff00'))}>C</div>
       </div>
+
+      <Board picture={picture} setPicture={setPicture}/>
     </div>
   );
 }
