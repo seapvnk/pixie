ed286959666087ffb3b557decbed5828cedc499e
Pedro Sérgio <www.psro@gmail.com>
2020-10-21 18:52:37 -0300

    App: handle new Board logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import Picture from './model/Picture';
 
 function App() {
 
-  const [picture, setPicture] = useState(Picture.empty(100, 100, '#ffffff00'))
+  const [picture, setPicture] = useState(Picture.empty(32, 32, '#ffffff00'))
 
   return (
     <div className="app">
       <div className="tools">
-        <div className="tool" onClick={() => setPicture(Picture.empty(100, 100, '#ffffff00'))}>C</div>
+        <div className="tool" onClick={() => setPicture(Picture.empty(32, 32, '#ffffff00'))}>C</div>
       </div>
 
       <Board picture={picture} setPicture={setPicture}/>
