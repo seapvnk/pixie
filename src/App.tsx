f0b6270a8070c7201ab5467ddefb3f6994326b9b
Pedro Sérgio <www.psro@gmail.com>
2020-10-22 12:24:05 -0300

    App: toolset

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,46 @@ import Picture from './model/Picture';
 
 function App() {
 
+  const [color, setColor] = useState('#5e315b')
   const [picture, setPicture] = useState(Picture.empty(32, 32, '#ffffff00'))
-
+  const pallete = [
+    '#5e315b', '#8c3f5d',  '#ba6156',
+    '#f2a65e',  '#ffe478',  '#cfff70',
+    '#8fde5d',  '#3ca370',  '#3d6e70',
+    '#323e4f',  '#322947',  '#473b78',
+    '#4b5bab',  '#4da6ff',  '#66ffe3',
+    '#ffffeb',  '#c2c2d1',  '#7e7e8f',
+    '#606070',  '#43434f',  '#272736',
+    '#3e2347',  '#57294b',  '#964253',
+    '#e36956',  '#ffb570',  '#ff9166',
+    '#eb564b',  '#b0305c',  '#73275c',
+    '#422445',  '#5a265e',  '#80366b',
+    '#bd4882',  '#ff6b97 '
+  ]
   return (
     <div className="app">
       <div className="tools">
         <div className="tool" onClick={() => setPicture(Picture.empty(32, 32, '#ffffff00'))}>C</div>
+        
+        <div className="tool" onClick={() => setColor('#ffffff00')}>E</div>
+        {pallete.map(color => {
+          return (
+            <div 
+              className="tool" 
+              onClick={() => setColor(color)}
+              style={{ background: color }}
+            >
+            </div>
+          )  
+        })}
+        
       </div>
 
-      <Board picture={picture} setPicture={setPicture}/>
+      <Board 
+        picture={picture} 
+        setPicture={setPicture} 
+        color={color}
+      />
     </div>
   );
 }
