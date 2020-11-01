d2c9782f89710254e0db9325e2aede49e8593cc4
Pedro Sérgio <www.psro@gmail.com>
2020-11-01 17:54:28 -0300

    Picture: draw method updated

--- src/model/Picture.ts
+++ src/model/Picture.ts
@@ -1,4 +1,4 @@
-interface PaintingProps {
+export interface PaintingProps {
     x: number;
     y: number;
     color: string;
@@ -32,42 +32,19 @@ class Picture {
         return newPicture;
     }
 
-    fillAt(x: number, y: number, color: string) {
-        const around = [
-            {dx: -1, dy: 0}, {dx: 1, dy: 0},
-            {dx: 0, dy: -1}, {dx: 0, dy: 1},
-        ];
-
-        const painted: Array<PaintingProps> = [{x, y, color}];
-        for (let done = 0; done < painted.length; done++) {
-            for (let {dx, dy}  of around) {
-                const x = painted[done].x + dx; 
-                const y = painted[done].y + dy;
-                
-                const betweenRange = (min: number, max: number) => (n: number) => n >= min && n < max;
-                const isPaintedAt = (x: number, y: number) => (p:PaintingProps) => p.x === x && p.y === y;
-                
-                const inRangeX = betweenRange(0, this.width);
-                const inRangeY = betweenRange(0, this.height);
-                const samePixelColor = this.pixel(x, y) === color;
-                const notPainted = !painted.some(isPaintedAt(x, y));
-
-                if (inRangeX(x) && inRangeY(y) && samePixelColor && notPainted) {
-                    painted.push({ x, y, color });
-                }
+    draw(picture: Picture, painted: Array<PaintingProps> = []): Picture {
+        if (painted.length > 0) {
+            const pixel = painted.pop();
+            if (pixel) {
+                const paintedPicture = this.setPixel(pixel?.x, pixel?.y, pixel?.color);
+                return this.draw(paintedPicture, painted);
             }
+
+            return picture;
+
+        } else {
+            return picture;
         }
-        
-        return this.draw(painted)
-    }
-
-    draw(painted: PaintingProps[]) {
-        let newPainting:Picture = this;
-        painted.forEach(({ x, y, color }) => {
-            newPainting = this.setPixel(x, y, color)
-        });
-
-        return newPainting;
     }
 
 }
